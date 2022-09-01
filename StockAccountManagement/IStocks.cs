bf0da82c8f13a8dfa137f6dbd5d962ee5257aec2
Prasad <banprasad21@gmail.com>
2022-09-01 08:37:14 +0530

    [Raghav] Add: stock account management problem

---
+++ StockAccountManagement/IStocks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement
+{
+    public interface IStocks
+    {
+        public void stockAccount();
+        public void sell();
+        public void buy();
+        public void save();
+
+    }
+}
---
+++ StockAccountManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace StockAccountManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StockManage stockManage = new StockManage();
+            stockManage.stockAccount();
+            stockManage.transaction();
+        }
+    }
+}
